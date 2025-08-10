9cfb720daec31b0baad86ab66e78b7836076f4ec
Daniel Vela <daniel@veladan.org>
2025-08-10 14:36:49 +0200

    first commit

---
+++ MarvelStatus/MarvelStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelStatusApp.swift
+//  MarvelStatus
+//
+//  Created by Daniel Vela on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
