3cdddc524361c2416bcf2d80c465efa51998a4e9
Daniel Vela <daniel@veladan.org>
2025-08-10 17:02:22 +0200

    .

---
+++ MarvelStatus/Sources/Utilities/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  MarvelStatus
+//
+//  Created by Daniel Vela on 10/8/25.
+//
+
+enum Secrets {
+    static let marvelPublicKey = "YOUR_PUBLIC_KEY"
+    static let marvelPrivateKey = "YOUR_PRIVATE_KEY"
+}
